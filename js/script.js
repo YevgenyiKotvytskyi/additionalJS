f0a71f2eb2b78d08907dc66d3a4c05a6122e1a76
Yevgenyi Kotvytskyi <yegeniy.kotvytskyi@gmail.com>
2020-06-29 08:48:23 +0300

    Start lesson 12 weekend

--- js/script.js
+++ js/script.js
@@ -1,150 +1,132 @@
-'use strict';
+const 
+    btnAddUser = document.getElementById('registerUser'),
+    btnAuthor = document.getElementById('login'),
+    btnLogin = document.getElementById('login'),
+    userHTML = document.getElementById('list');
+    userTitle = document.getElementById('username');
 
-/* jshint -W097 */
-/*global
-alert, confirm, console, prompt, document, localStorage
-*/
+Users = class{
+    constructor(key){
+        this.userList = [];
+        this.key = key;
+    }
+    
+    askNewUserHandler(event){
 
-const
+        let 
+            msg = '',
+            fio;
 
-    listChildHTML =
-        `
-<li class="todo-item">
-			<span class="text-todo"></span>
-			<div class="todo-buttons">
-				<button class="todo-remove"></button>
-				<button class="todo-complete"></button>
-			</div>
-</li>`;
+        do {
 
+            fio = prompt("Введите Имя_Фамилия через пробел." + msg);
+            if (fio === null) break;
+            
+            if ((fio.length - 1) === fio.split(' ').join('').length) break;
 
-let
-    listChildElement = document.createElement('div'),
-    completedChildElement = document.createElement('div'),
-    input = document.querySelector('.header-input'),
-    todoList = document.querySelector('ul.todo-list'),
-    todoCompleted = document.querySelector('ul.todo-completed'),
-    button = document.querySelector('form button'),
-    container = document.querySelector('.todo-container'),
+            msg = 'Нужет один пробел между именем и фамилией!';
 
-    appData = {
-        key: 'todoList',
-        todo: {
-            list: [],
-            complited: []
-        },
+        } while (true);
+        
+        const firstName = fio.split(' ')[0];
+        const lastName = fio.split(' ')[1];
 
-        writeList() {
-            this.todo.list = [];
-            this.todo.complited = [];
-            todoList.querySelectorAll('li').forEach(element => this.todo.list.push(element.innerText));
-            todoCompleted.querySelectorAll('li').forEach(element => this.todo.complited.push(element.innerText));
-            localStorage.setItem(this.key, JSON.stringify(appData.todo));
-        },
+        
+        let login = prompt('Введите логин');
+        if ((login === null) || (login.trim() === '')) return;
+        login = login.trim();
 
-        readList() {
-            const todo = JSON.parse(localStorage.getItem(this.key));
-            if (todo) {
-                for (const property in this.todo) {
-                    if (Array.isArray(todo[property])) {
-                        this.todo[property] = todo[property];
+        const password = prompt('Введите пароль');
+        if ((password === null) || (password.trim()==='') )  return;
+
+        const now = new Date();
+        const dateRegistration = now.toLocaleDateString('ru', 
+            {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'}) 
+            + ', ' + now.toLocaleTimeString('ru') ;
+        this.userList.push({firstName, lastName, login, password,dateRegistration});
+        this.saveToStorage();
+        this.showUsers();
+    }
+
+    saveToStorage(){
+        localStorage.setItem(this.key,JSON.stringify(this.userList));
+    }
+
+    loadFromStorage() {
+        this.userList = JSON.parse(localStorage.getItem(this.key));
+    }
+
+    showUsers() {
+
+        userHTML.innerHTML = '';
+        this.userList.forEach(element => {
+            const user = document.createElement('tr');
+            user.innerHTML = `
+            <td>${element.firstName}</td>
+            <td>${element.lastName}</td>
+            <td>${element.dateRegistration}</td>
+            <td><button id="${'usr-' + element.login}"  class="btn-delete">удалить</button><td>`;
+            userHTML.append(user);
+        });
+    }
+
+    readStorageHandler(){
+        this.loadFromStorage();
+        this.showUsers();
+    }
+
+    userDeleteHandler(event) {
+
+        if (event.target.className == 'btn-delete') {
+            const login = event.target.id.split('-')[1];
+            let modify = false;
+            if (login) {
+                const arr = this.userList;
+                for (let i = arr.length - 1; i > -1; i--) {
+                  
+                    if ( arr[i].login === login ) {
+                        arr.splice(i, 1);
+                        modify = true;
                     }
-                }
+                  }
             }
-        },
-
-        writeToHTML () {
-
-            todoList.innerHTML = '';
-            this.todo.list.forEach(element => {
-                const newElement = listChildElement.cloneNode(true);
-                newElement.querySelector('span.text-todo').innerText = element;
-                todoList.append(newElement);
-            });
-
-            todoCompleted.innerHTML = '';
-            this.todo.complited.forEach(element => {
-                const newElement = listChildElement.cloneNode(true);
-                newElement.querySelector('span.text-todo').innerText = element;
-                todoCompleted.append(newElement);
-            });
-
-        },
-
-        showLocalStorage() {
-            this.readList();
-            this.writeToHTML();
+            if (modify) {
+                this.showUsers();
+                this.saveToStorage();
+            }
         }
 
-    };
-
-listChildElement.innerHTML = listChildHTML;
-listChildElement = listChildElement.children[0];
-
-
-
-function addItem() {
-    const inputContent = input.value.trim();
-    input.value = '';
-    if (inputContent) {
-        const newElement = listChildElement.cloneNode(true);
-        newElement.querySelector('span.text-todo').innerText = inputContent;
-        todoList.append(newElement);
-        return true;
     }
-    return false;
-}
 
-let target;
+    loginHandler() {
 
-function addToCompleteItems(event) {
-    const target = event.target;
-    if (target.classList.contains('todo-complete')) {
-        let todoElement = target.closest('li.todo-item');
-        if (todoElement) {
-            const newCompleteItem = listChildElement.cloneNode(true);
-            todoCompleted.appendChild(todoElement);
+        const 
+            login = prompt('Введите логин'),
+            password = prompt('Введите пароль');
+        let notFound = true;
+        for (let element of this.userList) {
+            if ((element.login === login.trim()) && (element.password === password) ) {
+                userTitle.textContent = `${element.firstName} ${element.lastName}`;
+                notFound = false;
+                break;
+            }
         }
-    }
-}
 
-function addToCompleteItems(event) {
-    const target = event.target;
-    if (target.classList.contains('todo-complete')) {
-        let todoElement = target.closest('li.todo-item');
-        if (todoElement) {
-            const newCompleteItem = listChildElement.cloneNode(true);
-            todoCompleted.appendChild(todoElement);
+        if (notFound) {
+            alert ('Пользователь с таким логином и паролем не найден!');
         }
+
     }
-}
 
-function removeItem(event) {
-    const target = event.target;
-    if (target.classList.contains('todo-remove')) {
-        let todoElement = target.closest('li.todo-item');
-        if (todoElement) {
-            todoElement.remove();
-        }
-    }
-}
+    eventLiseners() {
 
-function storeData(event) {
-    const target = event.target;
-    if (target.tagName == 'BUTTON' &&
-        (target.classList.contains('todo-remove') ||
-            target.classList.contains('todo-complete') ||
-            target.classList.contains('header-button'))) {
-
-        if (target.classList.contains('header-button')) event.preventDefault();
-        appData.writeList();
+        btnAddUser.addEventListener('click',this.askNewUserHandler.bind(this));
+        window.addEventListener('load',this.readStorageHandler.bind(this));
+        document.addEventListener('click',this.userDeleteHandler.bind(this));
+        btnAuthor.addEventListener('click',this.loginHandler.bind(this));
 
     }
 }
 
-todoList.addEventListener('click', addToCompleteItems);
-container.addEventListener('click', removeItem);
-button.addEventListener('click', addItem);
-document.body.addEventListener('click', storeData);
-
-appData.showLocalStorage();
+users = new Users('userList');
+users.eventLiseners.call(users);
